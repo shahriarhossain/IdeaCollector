3655175ecf240ace7c5157d17d7fd8cfdb14f0b7
Shahriar Hossain <shahriar_cse@live.com>
2018-05-16 14:21:20 +0600

    use of router.route() method

--- routes/api.js
+++ routes/api.js
@@ -26,11 +26,6 @@ router.get('/About', (req, res)=>{
     res.render("About");
 })
 
-//get idea
-router.get('/AddIdea', (req, res)=>{
-    res.render("CreateIdea");
-})
-
 //View idea by ID
 //Use route parameter for essential values. /ViewIdea/:id 
 //We could pass multiple parameters too. /ViewIdea/:year/:month
@@ -40,53 +35,57 @@ router.get('/ViewIdea/:id', (req, res)=>{
     console.log(req.query);  
 })
 
-//set idea
-router.post('/AddIdea', (req, res)=>{ 
-    ////validation from scratch
-    // if(req.body.idea.title.length<3) 
-    // {
-    //     res.status(400).send("Title length must be 4 atleast");
-    //     return;
-    // }
+//get idea
+router.route('/AddIdea')
+      .get((req, res)=>{
+        res.render("CreateIdea");
+      })
+      .post((req, res)=>{ 
+          ////validation from scratch
+          // if(req.body.idea.title.length<3) 
+          // {
+          //     res.status(400).send("Title length must be 4 atleast");
+          //     return;
+          // }
 
-    ////validation with Joi middleware
-    // const validationSchema = {
-    //     body:{
-    //         idea:{
-    //             title: Joi.string().min(4).required(),
-    //             description: Joi.string().min(4)
-    //         }     
-    //     }
-    // };
+          ////validation with Joi middleware
+          // const validationSchema = {
+          //     body:{
+          //         idea:{
+          //             title: Joi.string().min(4).required(),
+          //             description: Joi.string().min(4)
+          //         }     
+          //     }
+          // };
 
-    // const validationResult = Joi.validate(req, validationSchema);
-  
-    // if(validationResult.error)
-    // {
-    //     res.status(400).send(validationResult.error.details[0].message);
-    //     return;
-    // }
+          // const validationResult = Joi.validate(req, validationSchema);
+        
+          // if(validationResult.error)
+          // {
+          //     res.status(400).send(validationResult.error.details[0].message);
+          //     return;
+          // }
 
-    const {error} = customValidation(req);
-  
-    if(error)
-    {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+          const {error} = customValidation(req);
+        
+          if(error)
+          {
+              res.status(400).send(error.details[0].message);
+              return;
+          }
 
-    //populating the model with data
-    const newIdea = {
-        title: req.body.idea.title,
-        description: req.body.idea.description
-    }
+          //populating the model with data
+          const newIdea = {
+              title: req.body.idea.title,
+              description: req.body.idea.description
+          }
 
-    new Idea(newIdea)
-        .save()
-        .then(idea=>{
-            res.redirect('/ideas');
-        })
-})
+          new Idea(newIdea)
+              .save()
+              .then(idea=>{
+                  res.redirect('/ideas');
+              })
+      })
 
 router.get('/Ideas', (req, res)=>{
     //fetch data from db
